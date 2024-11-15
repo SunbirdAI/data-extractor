1007582b3953a1d1e299acbde401a60039485fe0
Patrick Walukagga <pwalukagga@gmail.com>
2024-11-15 13:25:10 +0300

    Expose api endpoints

---
+++ docs.py
@@ -0,0 +1,13 @@
+description = """
+Welcome to the Acres AI RAG API documentation.
+
+### RAG Tasks
+- Use the `/process_zotero_library_items`: Process zotero library items with your zotero credentials.
+- Use the `/get_study_info`: Get number of documents in a zotero study.
+- Use the `/study_variables`: Get research summary from the study provided the study variables.
+- Use the `/download_csv`: Export the markdown text to a csv file.
+"""
+
+tags_metadata = [
+    {"name": "ACRES RAG", "description": "AI RAG Application"},
+]
